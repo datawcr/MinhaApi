4a67f0629656855762941767b78d3773ade01918
datawcr <datawcr@gmail.com>
2025-08-25 19:17:13 +0000

    Primeiro commit

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using MinhaApi.Models;
+
+namespace MinhaApi.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Produto> Produtos { get; set; }
+}
+
---
+++ Models/Produto.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using MyApp.Models; // ajuste para seu namespace
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+app.UseSwagger();
+app.UseSwaggerUI();
+}
+app.MapGet("/", () => "Hello World da API rodando!");
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
